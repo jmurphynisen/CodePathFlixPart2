091288cfee30f4b7465f1c379ca6a26f74081e62
John Murphy <jmurphynisen@gmail.com>
2021-02-17 23:28:10 -0500

    This adds movie grid view and detail screen

---
+++ CodePathFlix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  CodePathFlix
+//
+//  Created by Amy King on 2/17/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- CodePathFlix/MovieViewController.swift
+++ CodePathFlix/MovieViewController.swift
@@ -64,4 +64,13 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
             
         return cell
         }
+        
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MoviesDetailViewController; detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        }
     }
